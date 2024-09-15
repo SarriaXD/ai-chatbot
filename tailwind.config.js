b358fae5a76b02c7d25875a4285c6f4b6dd729b0
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-15 12:44:39 -0500

    remove src folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ import withMT from '@material-tailwind/react/utils/withMT'
 
 export default withMT({
     content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
         fontFamily: {
