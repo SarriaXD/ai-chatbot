c1dc1bf2525aded6777b09172acce2389a8eed3b
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-16 15:16:45 -0500

    change some styles of empty-message-placeholder.tsx

--- app/ui/chat/empty-message-placeholder.tsx
+++ app/ui/chat/empty-message-placeholder.tsx
@@ -2,11 +2,13 @@ import { Dog } from '@public/icons'
 
 const EmptyMessagePlaceholder = () => {
     return (
-        <div className="flex size-full flex-1 flex-col items-center justify-center gap-8 bg-gray-900 px-4">
-            <Dog className="size-64 text-white md:size-80" />
-            <p className="text-center text-xl text-gray-300 md:text-2xl">
-                Hi I am Qi, how can I help you today?
-            </p>
+        <div className="flex size-full items-center justify-center bg-gray-900 px-4">
+            <div className="max-w-60 md:max-w-80">
+                <Dog className="aspect-square text-white" />
+                <p className="mt-8 text-center text-xl text-gray-300 md:text-2xl">
+                    Hi I am Qi, how can I help you today?
+                </p>
+            </div>
         </div>
     )
 }
