80269d80e100bfaa686461f44a19b72e8463473a
SarriaXD <sarria.qi.wang@gmail.com>
2024-09-20 23:36:11 -0500

    open sidebar when we have user.

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -2,12 +2,19 @@
 
 import ChatSidebar from '@ui/chat/chat-sidebar.tsx'
 import ChatHeader from '@ui/chat/chat-header.tsx'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuth } from '@lib/client/hooks/use-auth.ts'
+import { useMediaQuery } from '@lib/client/hooks/use-media-query.ts'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     const { user } = useAuth()
-    const [open, setOpen] = useState(!!user)
+    const [open, setOpen] = useState(false)
+    const isMobile = useMediaQuery('(max-width: 735px)')
+    useEffect(() => {
+        if (!isMobile) {
+            setOpen(!!user)
+        }
+    }, [user, isMobile])
 
     return (
         <div className="flex size-full">
